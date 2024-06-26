f94cd4c262843c96bae464921e60dcb7d13e5de4
Yassel DG <yasseldg@gmail.com>
2024-06-26 11:02:19 +0000

    rest walletBalance

---
+++ rest/service/getWalletBalance.go
@@ -0,0 +1,55 @@
+package service
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/yasseldg/bybit/common"
+	"github.com/yasseldg/bybit/rest/models/account"
+	"github.com/yasseldg/bybit/rest/response"
+)
+
+type GetWalletBalance struct {
+	c *common.Client
+	r *common.Request
+
+	*account.WalletBalance
+}
+
+type GetWalletBalanceResponse struct {
+	common.RetResponse
+	Result response.WalletBalanceInfo `json:"result"`
+}
+
+func NewGetWalletBalance(c *common.Client, accountType string) (*GetWalletBalance, error) {
+	walletBalance, err := account.NewWalletBalance(accountType)
+	if err != nil {
+		return nil, err
+	}
+
+	request := new(common.Request).EndPoint("/v5/account/wallet-balance").Get().Signed()
+
+	return &GetWalletBalance{
+		c: c,
+		r: request,
+
+		WalletBalance: walletBalance,
+	}, nil
+}
+
+func (o *GetWalletBalance) Do(ctx context.Context, opts ...common.RequestOption) (*GetWalletBalanceResponse, error) {
+
+	o.r.SetParams(o.GetParams())
+
+	data, err := o.c.Call(ctx, o.r, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	res := new(GetWalletBalanceResponse)
+	err = json.Unmarshal(data, res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
